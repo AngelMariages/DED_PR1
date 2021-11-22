134462ca901971b4cfd00aeaf49fe1dd5141797b
Àngel Mariages <angel.mariages@gmail.com>
2021-11-22 19:35:21 +0100

    fix DateUtils was in test folder

--- src/main/java/uoc/ded/practica/util/DateUtils.java
+++ src/main/java/uoc/ded/practica/util/DateUtils.java
@@ -1,4 +1,29 @@
 package uoc.ded.practica.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
 public class DateUtils {
+
+    public static Date createDate(String stringDate) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+        try {
+            return sdf.parse(stringDate);
+        } catch (ParseException e) {
+            System.out.println("ERROR: Couldn't parsing date: " + stringDate);
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+
+    public static LocalDate createLocalDate(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        return LocalDate.parse(date, formatter);
+    }
 }
